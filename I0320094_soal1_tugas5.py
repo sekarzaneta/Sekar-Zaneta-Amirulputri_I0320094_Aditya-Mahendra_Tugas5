ceed519524834d2b68d8d750eee6c3a2434e8e32
Sekar Zaneta <masayusekar@gmail.com>
2021-04-01 19:17:27 +0700

    exercise

--- I0320094_soal1_tugas5.py
+++ I0320094_soal1_tugas5.py
@@ -1,1 +1,9 @@
+print("==== Menyapa Pengunjung Hotel ====")
 
+nama = input('Masukkan Nama: ')
+jenis_kelamin = input('Masukkan Jenis Kelamin (Laki laki/Perempuan): ')
+
+if jenis_kelamin == 'Laki laki':
+    print('Selamat datang, Mr', nama + '!')
+else:
+    print('Selamat datang, Mrs', nama + '!')
--- I0320094_soal2_tugas5.py
+++ I0320094_soal2_tugas5.py
@@ -1,1 +1,23 @@
+print("==== Mengkonversi Nilai ====")
 
+nama = input('Masukkan Nama: ')
+nilai = int(input('Masukkan Nilai: '))
+
+if nilai < 60 and nilai >=0:
+    nilai_akhir = "E"
+elif nilai <= 64 and nilai >= 60:
+    nilai_akhir = "C"
+elif nilai <= 69 and nilai >= 65:
+    nilai_akhir = "C+"
+elif nilai <= 74 and nilai >= 70:
+    nilai_akhir = "B"
+elif nilai <= 79 and nilai >= 75:
+    nilai_akhir = "B+"
+elif nilai <= 84 and nilai >= 80:
+    nilai_akhir = "A-"
+elif nilai <= 100 and nilai >= 85:
+    nilai_akhir = "A"
+else:
+    print("Maaf " + nama + ", nilai yang Anda masukkan tidak valid")
+
+print("Halo, " + nama + "! Nilai Anda setelah dikonversi adalah", nilai_akhir)
